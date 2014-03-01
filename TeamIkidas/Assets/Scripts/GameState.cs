37a35835dd5588f40b4e784c346549ff427f4ebe
Lars Huring <lars.huring@loveatwork.se>
2014-03-01 17:52:28 +0100

    Traversal between scenes

---
+++ TeamIkidas/Assets/Scripts/GameState.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	private static GameState _instance;
+
+	// Persistant variables
+	private string _currentScene;
+	private int _experience;
+	private int _hp;
+	private string _playerName;
+	private string _previousScene;
+
+	public static GameState Instance {
+		get {
+
+			if (_instance == null) {
+				_instance = new GameObject("GameState").AddComponent<GameState>();
+			}
+
+			return _instance;
+		}
+	}
+
+	public void OnApplicationQuit () {
+		_instance = null;
+	}
+
+	public void StartState() {
+
+		_currentScene = "Overworld";
+		_experience = 0;
+		_hp = 100;
+		_playerName = "My player name";
+
+		Application.LoadLevel ("Overworld");
+
+	}
+
+	// Set scene
+	public string currentScene {
+		get {
+			return _currentScene;
+		}
+	}
+
+	public string previousScene {
+		get {
+			return _previousScene;
+		}
+	}
+
+	// Set experience points
+	public void AddExperience(int points) {
+		_experience += points;
+	}
+
+	// Get experience
+	public int experience {
+		get {
+			return _experience;
+		}
+	}	
+
+	// Get player name
+	public string playerName {
+		get {
+			return _playerName;
+		}
+	}
+
+	public void LoadLevel(string scene) {
+
+		// Save previous scene to be able to return back
+		_previousScene = _currentScene;
+
+		// Update current scene
+		_currentScene = scene;
+		// Load current scene
+		Application.LoadLevel (_currentScene);
+	}
+}
---
+++ TeamIkidas/Assets/Textures/ActionScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionScript : MonoBehaviour {
+
+	public string SceneToLoadForward;
+	public string SceneToLoadBackward;
+	private bool isCollision = false;
+	private float verticalDirection;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (SceneToLoadForward);
+		Debug.Log (SceneToLoadBackward);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (isCollision) {
+			verticalDirection = Input.GetAxis("Vertical");
+
+
+
+			// Vertical up
+			if (SceneToLoadForward.Length > 0 && verticalDirection > 0) {
+
+				// Load scene forward
+				Debug.Log("Moving forward to scene: " + SceneToLoadForward);
+				GameState.Instance.LoadLevel(SceneToLoadForward);
+
+			
+			} else if (verticalDirection < 0) {
+
+				if (SceneToLoadBackward.Length > 0) {
+					GameState.Instance.LoadLevel(SceneToLoadBackward);
+				} else {
+					GameState.Instance.LoadLevel(GameState.Instance.previousScene);
+				}
+
+				// Load scene backwards
+				Debug.Log("Moving backward to scene: " + SceneToLoadBackward);
+			}
+
+				
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		SpecialEffectsHelper.Instance.MoveForward(transform.position);
+		isCollision = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		isCollision = false;
+		SpecialEffectsHelper.Instance.Destroy ();
+	}
+}
