208e3f25b014760c8ad7d6c25ac2d3001fcb1b91
Lars Huring <lars.huring@loveatwork.se>
2014-03-02 13:46:14 +0100

    Grafx

---
+++ TeamIkidas/Assets/Scripts/StarCollector/IkidasSimpleController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IkidasSimpleController : MonoBehaviour {
+
+	public float speed = 5;
+	private Animator animator;
+	private bool isMoving = false;
+	private Vector2 input;
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		input = new Vector2 (Input.GetAxis ("Horizontal") * Time.smoothDeltaTime * speed, Input.GetAxis ("Vertical") * Time.smoothDeltaTime * speed);
+
+		if (!isMoving && input != Vector2.zero) {
+			GameState.Instance.gameIsPaused = false;
+			isMoving = true;
+		}
+
+		if (input.y > 0) {
+			animator.enabled = true;
+			animator.SetInteger ("direction", 2);
+		} else if (input.y < 0) {
+			animator.enabled = true;
+			animator.SetInteger ("direction", 0);
+		} else if (input.x > 0) {
+			animator.enabled = true;
+			animator.SetInteger ("direction", 3);
+		} else if (input.x < 0) {
+			animator.enabled = true;
+			animator.SetInteger ("direction", 1);
+		} else {
+			animator.enabled = false;		
+		}
+
+		transform.Translate (input.x, input.y, 0, Space.Self);
+
+	}
+}
