506d64caabf072d623bb8a73fc5aebd8840a5d64
Lars Huring <lars.huring@loveatwork.se>
2014-03-02 12:09:34 +0100

    Added Ikidas animation

---
+++ TeamIkidas/Assets/IkidasController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IkidasController : MonoBehaviour
+{
+	
+	private Animator animator;
+	
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+		var vertical = Input.GetAxis("Vertical");
+		var horizontal = Input.GetAxis("Horizontal");
+		
+		if (vertical > 0) {
+			animator.enabled = true;
+			animator.SetInteger ("direction", 2);
+		} else if (vertical < 0) {
+			animator.enabled = true;
+			animator.SetInteger ("direction", 0);
+		} else if (horizontal > 0) {
+			animator.enabled = true;
+			animator.SetInteger ("direction", 3);
+		} else if (horizontal < 0) {
+			animator.enabled = true;
+			animator.SetInteger ("direction", 1);
+		} else {
+			animator.enabled = false;		
+		}
+	}
+}
